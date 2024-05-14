b6c46387c684d8d6dfdc975fe4921d90c53b06cf
Angelliz96 <angeliz9617@gmail.com>
2024-05-13 17:03:04 -0700

    data base working getAllBooks working

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,50 +1,51 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+// const Schema = mongoose.Schema;
+
+const bookSchema = new mongoose.Schema({
     title:{
         type: String,
-        required:true,
+        
 
     },
 author:{
     type: String,
-    require:true,
+    
 
 },
 publisher:{
     type: String,
-    require:true,
+    
 
 },
 genre:{
     type: String,
-    require:true,
+    
 
 },
 pages:{
     type: Number,
-    require:true, 
+    
 
 },
 rating:{
     type: Number,
-    require:true,
+    
 
 },
 synopsis:{
     type: String,
-    require:true,
+    
 
 },
 image:{
     type: String,
-    require:true,
+  
 
 }
 
 });
 
-const Comics = mongoose.model("Comics", bookSchema);
+const Books = mongoose.model("Book", bookSchema);
 
-module.exports = Comics;
+module.exports = Books;
